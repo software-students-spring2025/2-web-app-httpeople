9ab0b5905f0d1514b109d8d85d14901280b32bff
alw9411 <alw9411@nyu.edu>
2025-02-20 23:25:35 -0500

    add flask server file

--- server.py
+++ server.py
@@ -1,1 +1,8 @@
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+
+@app.route('/')
+def homepage():
+    pass  # put HTML for homepage here, with return render_template('homepage.html') or whatever the homepage is called
