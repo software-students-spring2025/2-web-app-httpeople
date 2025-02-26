0ed2717d18847d5664a99f3b12570b5c2f9fbd2f
bryantto08 <bryantto08@gmail.com>
2025-02-26 16:59:54 -0500

    implemented simple search page

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from db import db
 
 app = Flask(__name__)
@@ -8,11 +8,13 @@ app = Flask(__name__)
 def homepage():
     pass  # put HTML for homepage here, with return render_template('homepage.html') or whatever the homepage is called
 
-@app.route("/search")
+@app.route("/search", methods=["GET", "POST"])
 def search():
-    recipes = db.recipes.find({})
-    print(recipes)
-    print(recipes[0]["name"])
+    if request.method == "POST" and request.form.get("recipe_name"):
+        name = request.form.get("recipe_name")
+        recipes = db.recipes.find({"name": name})
+    else:
+        recipes = db.recipes.find({})
+        
     return render_template("search.html", recipes=recipes)
-
 app.run(debug=True)
